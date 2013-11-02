051e6b616f742afbd4d6e53f3a4dccdc263a8920
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-11-02 08:24:03 +0100

    configure editor

--- public/js/asteroids.js
+++ public/js/asteroids.js
@@ -2,5 +2,8 @@
     var code = document.getElementById('code');
     code.textContent = 'function(){ /* do nothing */ }'
 
-    var editor = CodeMirror.fromTextArea(code);
+    var editor = CodeMirror.fromTextArea(code, {
+	mode: 'javascript',
+	lineNumbers: true
+    });
 })(CodeMirror)
