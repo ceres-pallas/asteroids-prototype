cd12880a1b401e44dd1326f6b2486c6cf2138868
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-11-02 08:45:57 +0100

    draw fighter

--- public/js/asteroids.js
+++ public/js/asteroids.js
@@ -6,4 +6,15 @@
 	mode: 'javascript',
 	lineNumbers: true
     });
+
+    var canvas = document.getElementById('top');
+    context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.moveTo(10,10);
+    context.lineTo(15, 30);
+    context.lineTo(20, 10);
+    context.closePath();
+
+    context.fill();
 })(CodeMirror)
