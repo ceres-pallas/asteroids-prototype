3a5fc296aa08efaf4fe09aa20490863f27c6171c
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-04 09:31:12 +0100

    serve static content

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = new express();
 
+app.use('/static', express.static(__dirname + '/public'));
+
 app.set('port', process.env.port || 1729);
 app.listen(app.get('port'));
 
