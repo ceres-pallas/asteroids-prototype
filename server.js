d9e7a5677200a42217112e459f25dcd01b75e8d8
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-04 09:39:47 +0100

    incorporate socket.io

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = new express();
 
-app.use('/static', express.static(__dirname + '/public'));
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.set('port', process.env.port || 1729);
-app.listen(app.get('port'));
+app.use('/static', express.static(__dirname + '/public'));
+
+server.listen(app.get('port'));
 
 console.log('server started at port %s', app.get('port'));
